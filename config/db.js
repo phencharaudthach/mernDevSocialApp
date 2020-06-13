10058a286409c3e99365cde49016265e8d6d018e
Phen Thach <pshannonthach@gmail.com>
2020-06-13 18:15:01 -0400

    Added GET to profile route to get all profiles or by using a user ID

--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
+      useFindAndModify: true,
     });
 
     console.log('MongoDB Connected');

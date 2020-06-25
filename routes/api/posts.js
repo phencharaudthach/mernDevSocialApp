6df47ce75ae60c8705a735da27ae4e101fdde0aa
Phencharaud S Thach <pshannonthach@gmail.com>
2020-06-24 22:46:29 -0400

    moving into react now

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,8 +44,8 @@ router.post(
 
 router.get('/', auth, async (req, res) => {
   try {
-    const post = await Post.find().sort({ date: -1 });
-    res.json(post);
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');

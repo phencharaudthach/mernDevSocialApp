46f055f31900e17571626ba8322ebe18e86d210e
Phencharaud S Thach <pshannonthach@gmail.com>
2020-06-30 22:22:46 -0400

    completed private route for dashboard for only logged in users

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+// fetch all of our data using an action and bring it in from the redux state and pass it down to other components ex, experience and education
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = (props) => {
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;

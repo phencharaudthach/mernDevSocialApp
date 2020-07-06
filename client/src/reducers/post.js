58aea145ec8d0ecc5fe7620c0701906b3ab78fe2
Phencharaud S Thach <pshannonthach@gmail.com>
2020-07-06 17:06:41 -0400

    done reducer/action to delete post

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,9 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,6 +22,12 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
@@ -26,7 +37,7 @@ export default function (state = initialState, action) {
     case UPDATE_LIKES:
       return {
         ...state,
-        posts: state.post.map((post) =>
+        posts: state.posts.map((post) =>
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
         loading: false,

ffba320c588092edab61321b6a02b61e3a80df70
phencharaudthach <53065868+phencharaudthach@users.noreply.github.com>
2020-07-08 23:21:44 -0400

    completing add post action, reducer, and component

---
+++ client/src/components/posts/PostForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addPost } from '../../actions/post';
+
+const PostForm = (props) => {
+  return <div></div>;
+};
+
+PostForm.propTypes = {};
+
+export default PostForm;
